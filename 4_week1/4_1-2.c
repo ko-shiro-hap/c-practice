d331e7a433ded1b80cc7a310668b1161b78ce432
ko-shiro-hap <koushirou.iino@icloud.com>
2023-01-01 20:12:25 +0900

    4_week1-2

---
+++ 4_week1/4_1-2.c
@@ -0,0 +1,24 @@
+# include <stdio.h>
+
+int fuzzyStrcmp(char s[], char t[]){
+	//関数の中だけを書き換えてください
+	//同じとき１を返す，異なるとき０を返す
+  for(int i=0; s[i]==t[i] || s[i]==t[i]+32 || s[i]==t[i]-32; i++){
+    if(s[i]=='\0'){
+      return 1;
+    }
+  }
+  return 0;
+}
+
+//メイン関数は書き換えなくてできます
+int main(){
+    int ans;
+    char s[100];
+    char t[100];
+    scanf("%s %s",s,t);
+    printf("%s = %s -> ",s,t);
+    ans = fuzzyStrcmp(s,t);
+    printf("%d\n",ans);
+    return 0;
+}
